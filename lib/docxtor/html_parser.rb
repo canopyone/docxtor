feec6d3b5a5d48ff2c3c6b074a09d6a79137ce6c
jafrog <howeveririna@gmail.com>
2013-02-15 14:07:30 +0400

    HTMLParser methods for special parts + tests

--- lib/docxtor/html_parser.rb
+++ lib/docxtor/html_parser.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 
 module Docxtor
+  # HTML templates parser. Represents data in a hash, translates tags names to +.docx+ terms.
+  # Kinda.
   class HTMLParser
     attr_accessor :data, :template
 
     def initialize options = {}
-      @data = []
+      @data = {:document => []}
       @options = options
     end
 
@@ -13,24 +15,48 @@ module Docxtor
       doc = Nokogiri::HTML(template)
       nodes = doc.css('body')[0].children
 
-      extract_data nodes
-
-      #TODO: Table of contents, footers and headers parser
+      extract_document nodes
 
       data
     end
 
+    # TODO: to closely tied with Nokogiri::XML::Node?
+    def paragraph? node
+      ["div", "p"].include? node.name
+    end
+
+    def heading? node
+      node.name =~ /^h\d$/
+    end
+
+    def header? node
+      node.document.css(@options[:header]).include? node
+    end
+
+    def toc? node
+    end
+
     private
 
-    def extract_data nodes, append_to = data
+    def extract_document nodes, append_to = data[:document]
       nodes.each do |node|
-        key = node.name.to_sym
+        key = translate node
         append_to << {key => []}
-        if node.children.empty?
+        if node.element_children.empty?
           append_to.last[key] << node.text
         else
-          extract_data(node.children, append_to.last[key])
+          extract_document(node.children, append_to.last[key])
         end
+      end
+    end
+
+    def translate node
+      if paragraph? node
+        key = :p
+      elsif heading? node
+        key = node.name.to_sym
+      else
+        key = :run # run of text (see standart)
       end
     end
   end
--- test/test_docxtor_html_parser.rb
+++ test/test_docxtor_html_parser.rb
@@ -4,6 +4,8 @@ class TestDocxtorHTMLParser < Test::Unit::TestCase
   context "Parsing html template" do
     setup do
       @parser = Docxtor::HTMLParser.new
+      @template = File.read(File.join(File.dirname(__FILE__), 'support', 'template.html'))
+      @doc = Nokogiri::HTML(@template)
     end
 
     should "respond to #parse" do
@@ -12,8 +14,19 @@ class TestDocxtorHTMLParser < Test::Unit::TestCase
 
     context "#parse" do
       should "extract context variables from html template" do
-        template = "<html><body><div id='content'>Hello, docxtor!</div></body></html>"
-        assert_equal([{:div => [{:text => ["Hello, docxtor!"]}]}], @parser.parse(template))
+        assert_equal({:document => [{:h1 => ["Heading"]}, {:p => [{:run => ["Hello, docxtor!"]}, {:p => ["Nice to meet you!"]}]}]}, @parser.parse(@template))
+      end
+    end
+
+    context "#paragraph?" do
+      should "define whether given node should be translated to paragraph element" do
+        assert @parser.paragraph? @doc.css('div')[0]
+      end
+    end
+
+    context "#heading?" do
+      should "define wheter given node should be translated to heading element" do
+        assert @parser.heading? @doc.css('h1')[0]
       end
     end
   end
